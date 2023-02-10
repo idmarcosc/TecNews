f6fc5063626f8d129488702cc7717c6ee230de74
idmarcosc <92194475+idmarcosc@users.noreply.github.com>
2023-02-10 11:17:58 -0300

    Finalização

--- js/menu-mobile.js
+++ js/menu-mobile.js
@@ -1,10 +1,10 @@
-function menuMobile() {
-    let menuMobilee = document.querySelector('.nav-bar');
+function menuShow() {
+    let menuMobile = document.querySelector('.nav-bar');
     let iconMenu = document.getElementById("icon-menu").innerHTML;
-    if(menuMobilee.classList.contains('open')) {
-        menuMobilee.classList.remove('open');
+    if(menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
     } else {
-        menuMobilee.classList.add('open');
+        menuMobile.classList.add('open');
     }
     if (iconMenu == 'menu') {
         document.getElementById("icon-menu").innerHTML = "close";
